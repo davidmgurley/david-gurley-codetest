ee656e057247c545e1daf15254adf89f597eaf7b
Greg <ginman86@gmail.com>
2020-03-02 17:58:36 -0700

    Add more data and instructions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import MarkdownIt from 'markdown-it'
+import { succulents } from './succulents'
+
+const md = new MarkdownIt()
+const readme = md.render(`
+### Introduction
+
+This is a front-end focused code evaluation. You will have 3 hours to work on this followed by a 1 hour code review/walkthrough of what you have built.
+
+You are not expected to finish everything. Work with whatever tools and frameworks you would like, though a React base is preferred.
+
+### Story
+We have a collection of succulents, with different details about each variety. We want to give our users a way to browse these succulents and view details about them as desired.
+
+##### Requirements
+- A user can view all succulents in the collection at once
+- A user can view details on a specific succulent
+- A user can filter the list of succulents by name (text search)
+- A user can filter the list of succulents by type
+- A user can "like" (and subsequently revoke the like) for any number of succulents
+
+### Get started
+The app is bootstrapped using \`create-react-app\`. You can start the development server via \`yarn start\` or \`npm start\`. For more information on \`create-react-app\`, see the \`CRA-README.md\` file.
+
+\`App.js\` is your main entry point to the application.
+
+### Where to get the data
+In this repo is a file with an array of succulent data \`src/succulent.js\`. You can consume it however you would like. Feel free to move it.
+
+### Tests
+Tests are always welcome and at least one test as a proof of concept would be ideal.
+
+### Help/Questions
+Please reach out to ginman@thezerocard.com if you have any questions.
+`)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-content'>
+      <p>Edit App.js and save to reload</p>
+      <div dangerouslySetInnerHTML={{ __html: readme }} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
